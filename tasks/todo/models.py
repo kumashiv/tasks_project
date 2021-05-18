19e8c7a6e6433e2e4bf789048cdd54f59cfbec6b
shiv kumar <kumashiv_sk@outlook.com>
2021-05-17 22:02:53 -0400

    Thing model

--- tasks/todo/models.py
+++ tasks/todo/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+from django.core.validators import MinLengthValidator
+
+
+class Thing(models.Model):
+    name = models.CharField(
+            max_length=200,
+            help_text='Enter a category (e.g. School)',
+            validators=[MinLengthValidator(2, "Make must be greater than 1 character")]
+    )
+
+    def __str__(self):
+        """String for representing the Model object."""
+        return self.name
