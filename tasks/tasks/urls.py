e781c8eb53edf1e80f0d50a79fd79f747ca11ee9
Shiv Kumar <kumarshiv_sk@outlook.com>
2021-06-13 16:44:13 -0400

    improved queryset

--- tasks/tasks/urls.py
+++ tasks/tasks/urls.py
@@ -13,8 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+import os
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
 from django.conf.urls import url
 from django.views.generic import TemplateView
 
@@ -23,4 +26,5 @@ urlpatterns = [
     path('', TemplateView.as_view(template_name='home/main.html')),
     
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
